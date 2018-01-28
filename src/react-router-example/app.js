103911f8ab771432203c57ce0b6eb74225d8fdaf
Sarath <sarath.hacks@gmail.com>
2018-01-28 17:04:33 +0530

    :car: nested route

--- src/react-router-example/app.js
+++ src/react-router-example/app.js
@@ -9,6 +9,7 @@ import Home from './pages/home';
 import About from './pages/about';
 import Topics from './pages/topics';
 import SingleTopic from './pages/single-topic';
+import Profile from './pages/profile';
 
 
 const App = () => (
@@ -17,11 +18,13 @@ const App = () => (
             <div>
                 <Link to="/" >Home</Link>
                 <Link to="/about" >About</Link>
+                <Link to="/profile" >Profile</Link>
                 <Link to="/topics" >Topics</Link>
                 <Route path="/" exact component={ Home } />
                 <Route path="/about" exact component={ About } />
                 <Route path="/topics" component={ Topics } />
                 <Route path="/topics/:name" component={ SingleTopic } />
+                <Route path="/profile" component={ Profile } />
             </div>
         </Router>
     </div>
