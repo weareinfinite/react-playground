2ad99b4ad7aef56db48589f829cd0feb215c53d4
Sarath <saratonite@gmx.com>
2018-05-29 15:01:03 +0530

    :horse: modal animation

--- src/react-modal-example/modal.js
+++ src/react-modal-example/modal.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import './modal.css';
+
+import posed from 'react-pose';
+console.log(posed)
+
+const ModalBox = posed.div({
+    off: { scale: .5, x: 0, opacity: .5 },
+    on: { scale: 1, x: '5%', opacity: 1 }
+});
 export default class Modal extends Component {
 
     constructor(props) {
@@ -10,14 +18,14 @@ export default class Modal extends Component {
 
         const { show } = this.props;
 
-        if(!show) return(<span></span>)
+        
 
         return (
-            <div className="modal-overlay" >
-              <div className="modal">
+            <div className={show ? 'modal-overlay active' : 'modal-overlay'}>
+              <ModalBox className="modal" pose={show ? "on" : "off"}>
                 <h1>This is the modal</h1>
                 <button onClick={ this.props.handleClose }>Close </button>
-              </div>
+              </ModalBox>
             </div>
         );
     }
