9551ffe87dc7e6586a240847006a43f257313597
Sarath <sarath.hacks@gmail.com>
2019-06-20 11:50:28 +0530

    Using context api with contextType property

--- src/react-context-example/index.js
+++ src/react-context-example/index.js
@@ -16,6 +16,7 @@ const Mission = props => (
   <div style={{ border: "1px dotted #f56", padding: "10px" }}>
     The Mission
     <Secret />
+    <BetterScrete />
   </div>
 );
 
@@ -30,6 +31,17 @@ class Secret extends React.Component {
             </div>
           )}
         </TheContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class BetterScrete extends React.Component {
+  static contextType = TheContext;
+  render() {
+    return (
+      <div style={{ border: "1px solid violet", color: "lightgrey" }}>
+        <h3>> {this.context} </h3>
       </div>
     );
   }
